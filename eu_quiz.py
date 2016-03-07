c442c3f3e85f6e62ed019973e6b790fa18cd582d
taviur <tavia.davies@hotmail.co.uk>
2016-03-07 19:43:36 +0000

    wot am i doin

---
+++ eu_quiz.py
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+from flask import Flask 
+from flask import render_template 
+
+app = Flask("MyQuiz")
+
+@app.route("/")
+def home():
+	return render_template("index.html")
+
+app.run(
+	debug=True)
+=======
+boop
+
+boomshakalak
+
+hi :)
+>>>>>>> master
