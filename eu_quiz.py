cbd7bdf340127cf164c55194071ce8fe28a7f74a
Zita3 <zitaabila@gmail.com>
2016-03-09 21:58:16 +0000

    Changed template ref name to index.html

--- eu_quiz.py
+++ eu_quiz.py
@@ -11,15 +11,21 @@ app = Flask("My App")
 #the 'app' is just the variable name from above
 
 def eu_quiz():
-	return render_template("index_eu1.html")
+	return render_template("index.html")
 #Flask automatically looks for html templates in a folder called 'templates'. 
 #Make sure you have a folder called 'templates'
 
 @app.route("/take-quiz", methods=['POST'])
-def quiz_results():
+def quiz_data_collection():
 	form_data = request.form
 	print int(form_data['Q1'])
 	return "All OK"
+
+def results_addition():
+	return 
+
+def results_presentation():
+
 
 #'debug = True' allows it to update itself without you having to re-run it in the terminal/shell
 #port=xxxx changes the port so that you would now find the page at: http://127.0.0.1:3672/ <-- for example.
