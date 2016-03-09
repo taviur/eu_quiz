057997a588e97285f749f5b368a1421368284058
Zita3 <zitaabila@gmail.com>
2016-03-09 21:44:18 +0000

    Form and slider
    Created form with 2 test questions in an unordered list and using a HTML
    slider

--- eu_quiz.py
+++ eu_quiz.py
@@ -1,20 +1,28 @@
-from flask import Flask 
-from flask import render_template 
+#Code First Girls lesson 3 on 11 Feb 16 and lesson 4 on 19 Feb 16
+import requests
+from flask import Flask
+from flask import render_template
+from flask import request
 
-app = Flask("MyQuiz")
+app = Flask("My App")
 
 @app.route("/")
-def home():
-	return render_template("index.html")
+#the '/' just means the stem i.e. the home page
+#the 'app' is just the variable name from above
 
-@app.route("/quiz")
-def quiz():
-	return render_template("quiz.html")
+def eu_quiz():
+	return render_template("index_eu1.html")
+#Flask automatically looks for html templates in a folder called 'templates'. 
+#Make sure you have a folder called 'templates'
 
-@app.route("/results")
-def results():
-	return render_template("results.html")
+@app.route("/take-quiz", methods=['POST'])
+def quiz_results():
+	form_data = request.form
+	print int(form_data['Q1'])
+	return "All OK"
 
+#'debug = True' allows it to update itself without you having to re-run it in the terminal/shell
+#port=xxxx changes the port so that you would now find the page at: http://127.0.0.1:3672/ <-- for example.
+#app.run() needs to go after all the functions, otherwise the functions will not be run!
 app.run(
-	debug=True)
-
+	debug=True, port=3672)
